5b998affd7df52e021d9188d3ecb0d5288f197d6
Nathan Chacon <nathanchacon321@gmail.com>
2019-10-08 19:00:53 -0300

    Updates

---
+++ precache-manifest.3731bcca5eb80d27ac9470ad51d7bbd5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f845b8c317537555188149f3cdace6ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "404a2d9255d035cfb4a3",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "e7645a7da7e893ef3647",
+    "url": "/static/css/main.ca6e21f4.chunk.css"
+  },
+  {
+    "revision": "404a2d9255d035cfb4a3",
+    "url": "/static/js/2.fe55e438.chunk.js"
+  },
+  {
+    "revision": "e7645a7da7e893ef3647",
+    "url": "/static/js/main.33be4058.chunk.js"
+  },
+  {
+    "revision": "608567b3a286af875d2a",
+    "url": "/static/js/runtime~main.1dc4db23.js"
+  },
+  {
+    "revision": "e9fe3745557f609d4a73bf817edd52df",
+    "url": "/static/media/books.e9fe3745.jpg"
+  },
+  {
+    "revision": "91287a5c26f67d031cd8375fa7312682",
+    "url": "/static/media/perfilLink.91287a5c.jpg"
+  },
+  {
+    "revision": "a7639264937062dfa9516e6314d93e3c",
+    "url": "/static/media/planet.a7639264.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nathanchacon.github.io/precache-manifest.faf721b0514590c9b67ba687ecbbed61.js"
+  "/precache-manifest.3731bcca5eb80d27ac9470ad51d7bbd5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nathanchacon.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
